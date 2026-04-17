9ba1fa1cfede7713d278be3cd5072aedafae060c
Rasyid <sulaiman.rasyid20@gmail.com>
2026-04-17 18:12:49 +0700

    Add assignments 02

---
+++ assignments/02-numeric-operator/2.js
@@ -0,0 +1,14 @@
+let numeratorString = "9.5 x 4.5 − 2.5 x 3";
+let denominatorString = "45.5 - 35";
+
+console.log(`
+9.5 x 4.5 − 2.5 x 3
+─────────────────────
+       45.5 − 35
+`);
+
+numerator = 9.5 * 4.5 - 2.5 * 3;
+denominator = 45.5 - 35;
+result = (numerator) / (denominator);
+
+console.log(result);
